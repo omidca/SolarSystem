56d04e9b2216f45eca7f70ce457f64e91fd85722
omid1 <omid.sst22@gmail.com>
2022-08-29 09:23:27 -0700

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        solarSystem inception=new solarSystem();
+        inception.Sun=1;
+        inception.Moon=4;
+        inception.Planets=2;
+
+
+        SUN omaro=new SUN();
+        omaro.temperature = "123123";
+        omaro.name = "ferro";
+        omaro.radius = 123456;
+        omaro.color = "pink";
+
+
+        Planet1 superPlanet=new Planet1();
+        superPlanet.color="green";
+        superPlanet.name="hash";
+        superPlanet.size="123456ml";
+
+    }
+}
---
+++ src/com/company/SUN.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class SUN {
+
+    String temperature;
+    String name;
+    int radius;
+    String color;
+
+}
